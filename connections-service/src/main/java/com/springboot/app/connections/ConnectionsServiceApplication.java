8fc8b25e5b1e429044d8a53169bf13ff6c985e90
Unai Perez <uperez@certiorem.com>
2020-03-23 08:16:25 +0100

    modificaciones para usar libreria

--- connections-service/src/main/java/com/springboot/app/connections/ConnectionsServiceApplication.java
+++ connections-service/src/main/java/com/springboot/app/connections/ConnectionsServiceApplication.java
@@ -2,8 +2,10 @@ package com.springboot.app.connections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.springboot.app.commons.models.entity"})
 public class ConnectionsServiceApplication {
 
 	public static void main(String[] args) {
